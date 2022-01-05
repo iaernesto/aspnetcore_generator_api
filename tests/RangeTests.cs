2f074c19fec43ea9d4cecf7626af9cc34f0852d6
iaernesto <ernesto.gonzalez@ia.ca>
2022-01-05 07:05:21 -0500

    add sort test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -16,6 +16,17 @@ namespace tests
 
             Assert.Equal(3, generated.Count());
         }
+
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new api.Controllers.Range { Count = 3, Sort = true };
+            var values = new[] {"a","c", "b"};
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+
+            Assert.Equal(new[]{"a","c", "b"}, generated.ToArray());
+        }
     }
 }
 
